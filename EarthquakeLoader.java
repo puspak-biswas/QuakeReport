30dc796e04942d20bd9e1672a75c666429afc7f2
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-05-10 23:55:32 +0530

    Update EarthquakeLoader.java

--- EarthquakeLoader.java
+++ EarthquakeLoader.java
@@ -1,11 +1,21 @@
-public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+package com.example.android.quakereport;
 
-    private String URL
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by Puspak Biswas on 10-05-2018.
+ */
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+    private String URL;
 
     public EarthquakeLoader(Context context, String url) {
         super(context);
-	URL = url;
-        // TODO: Finish implementing this constructor 
+        URL = url;
+        // TODO: Finish implementing this constructor
     }
 
     @Override
@@ -15,12 +25,12 @@ public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 
     @Override
     public List<Earthquake> loadInBackground() {
-           List<Earthquake> earthquakes = null;
-            if(URL != null) {
-                // Create a fake list of earthquake locations.
-                earthquakes = QueryUtils.fetchEarthquakeData(URL);
-            }
-                return earthquakes; 
+        List<Earthquake> earthquakes = null;
+        if(URL != null) {
+            // Create a fake list of earthquake locations.
+            earthquakes = QueryUtils.fetchEarthquakeData(URL);
+        }
+        return earthquakes;
         // TODO: Implement this method
     }
 }
